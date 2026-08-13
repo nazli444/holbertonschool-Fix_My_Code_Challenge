f122f1023f9e8b68a25c87e2514b86856be38069
Your Name <you@example.com>
2026-08-13 18:03:33 +0000

    0-fizzbuzz.py

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python3
-
+"""
+Fix My Code Challenge - FizzBuzz
+"""
 import sys
 
-for i in range(1, int(sys.argv[1]) + 1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz", end=" ")
-    elif i % 3 == 0:
-        print("Fizz", end=" ")
-    elif i % 5 == 0:
-        print("Buzz", end=" ")
-    else:
-        print(i, end=" ")
-print()
+
+def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+    - multiples of 3 printed as 'Fizz'
+    - multiples of 5 printed as 'Buzz'
+    - multiples of both 3 and 5 printed as 'FizzBuzz'
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            tmp_result.append("FizzBuzz")
+        elif i % 3 == 0:
+            tmp_result.append("Fizz")
+        elif i % 5 == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+
+    print(" ".join(tmp_result))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        sys.exit(1)
+
+    fizzbuzz(int(sys.argv[1]))
