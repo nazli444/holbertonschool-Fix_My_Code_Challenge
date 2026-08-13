a4dd3ff88811de0a1d0978379ac0bd89e1e46ebf
Your Name <you@example.com>
2026-08-13 17:30:05 +0000

    0-fizzbuzz.py

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-if i % 3 == 0 and i % 5 == 0:
-    print("FizzBuzz", end=" ")
-elif i % 3 == 0:
-    print("Fizz", end=" ")
-elif i % 5 == 0:
-    print("Buzz", end=" ")
-else:
-    print(i, end=" ")
+import sys
+
+for i in range(1, int(sys.argv[1]) + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    elif i % 5 == 0:
+        print("Buzz", end=" ")
+    else:
+        print(i, end=" ")
