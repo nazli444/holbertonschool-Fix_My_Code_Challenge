eed2d8e49aab2d676d02ab157dd442b7c40de1eb
Your Name <you@example.com>
2026-08-13 18:24:39 +0000

    lists.h

---
+++ challenge/4-delete_dnodeint/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+void free_dlistint(dlistint_t *head);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
