a9ca17fedb5bb0cce98e4cbacff646b7fbf24196
Your Name <you@example.com>
2026-08-13 18:22:05 +0000

    print_dlistint.c

---
+++ challenge/4-delete_dnodeint/print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints all elements of a dlistint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t count = 0;
+
+	current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
